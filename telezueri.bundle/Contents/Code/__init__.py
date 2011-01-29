5a9e655ddad5942b99af26cdb568fcde36b77830
Christian Hitz <christian@klarinett.li>
2011-01-29 16:40:54 +0100

    update to framework v2

--- telezueri.bundle/Contents/Code/__init__.py
+++ telezueri.bundle/Contents/Code/__init__.py
@@ -1,7 +1,3 @@
-# PMS plugin framework
-from PMS import *
-from PMS.Objects import *
-from PMS.Shortcuts import *
 from random import randint
 
 ####################################################################################################
@@ -13,70 +9,32 @@ TELE_ZUERI_SERVERLIST_URL = "http://server072.20min-tv.ch/lb_20min/servers.xml?8
 
 NAME = L('Title')
 
-# make sure to replace artwork with what you want
-# these filenames reference the example files in
-# the Contents/Resources/ folder in the bundle
 ART           = 'art-default.png'
 ICON          = 'icon-default.png'
 
 ####################################################################################################
 
 def Start():
-
-    ## make this plugin show up in the 'Video' section
-    ## in Plex. The L() function pulls the string out of the strings
-    ## file in the Contents/Strings/ folder in the bundle
-    ## see also:
-    ##  http://dev.plexapp.com/docs/mod_Plugin.html
-    ##  http://dev.plexapp.com/docs/Bundle.html#the-strings-directory
-    Plugin.AddPrefixHandler(VIDEO_PREFIX, VideoMainMenu, L('Title'), ICON, ART)
+    Plugin.AddPrefixHandler(VIDEO_PREFIX, VideoMainMenu, NAME, ICON, ART)
 
     Plugin.AddViewGroup("InfoList", viewMode="InfoList", mediaType="items")
     Plugin.AddViewGroup("List", viewMode="List", mediaType="items")
 
-    ## set some defaults so that you don't have to
-    ## pass these parameters to these object types
-    ## every single time
-    ## see also:
-    ##  http://dev.plexapp.com/docs/Objects.html
     MediaContainer.art = R(ART)
-    MediaContainer.title1 = NAME
+    #MediaContainer.title1 = NAME
     DirectoryItem.thumb = R(ICON)
 
-  
-
-
-#### the rest of these are user created functions and
-#### are not reserved by the plugin framework.
-#### see: http://dev.plexapp.com/docs/Functions.html for
-#### a list of reserved functions above
-
-
-
-#
-# Example main menu referenced in the Start() method
-# for the 'Video' prefix handler
-#
-
 def VideoMainMenu():
-
-    # Container acting sort of like a folder on
-    # a file system containing other things like
-    # "sub-folders", videos, music, etc
-    # see:
-    #  http://dev.plexapp.com/docs/Objects.html#MediaContainer
     dir = MediaContainer(viewGroup="InfoList")
 
-
-    # see:
-    #  http://dev.plexapp.com/docs/Objects.html#DirectoryItem
-    #  http://dev.plexapp.com/docs/Objects.html#function-objects
-
-    serverlist = XML.ElementFromURL(TELE_ZUERI_SERVERLIST_URL, cacheTime=300)
+    serverlist = HTML.ElementFromURL(TELE_ZUERI_SERVERLIST_URL, cacheTime=300)
+    Log(XML.StringFromElement(serverlist))
     servers = serverlist.xpath('//var[@name="url"]/string')
     for server in servers:
-        result = HTTP.Request(server.text)
-        if not result:
+        Log(server.text)
+        try:
+            result = HTTP.Request(server.text)
+        except:
             servers.remove(server)
 
     videolist = XML.ElementFromURL(TELE_ZUERI_VIDEOLIST_URL, cacheTime=120)
@@ -85,12 +43,12 @@ def VideoMainMenu():
     for show in shows:
         title = show.xpath('var[@name="title"]/string')[0].text
         ident = show.xpath('var[@name="id"]/number')[0].text
-        dir.Append(Function(DirectoryItem(ShowMenu,title), server=servers[(randint(0, len(servers) - 1))].text, ident=ident))
+        dir.Append(Function(DirectoryItem(ShowMenu, title), server=servers[(randint(0, len(servers) - 1))].text, ident=ident))
 
     return dir
 
 def ShowMenu(sender, ident, server, start=0):
-    dir = MediaContainer(viewGroup="InfoList")
+    dir = MediaContainer(viewGroup="InfoList", title2=sender.itemTitle)
     show_count = 0
 
     videolist = XML.ElementFromURL(TELE_ZUERI_VIDEOLIST_URL, cacheTime=120)
@@ -104,7 +62,7 @@ def ShowMenu(sender, ident, server, start=0):
         title = episode.xpath('var[@name="title"]/string')[0].text
         episode_id = episode.xpath('var[@name="id"]/number')[0].text
 
-        details = XML.ElementFromURL(TELE_ZUERI_DETAILS_URL, values={"channel_id": ident, "video_id": episode_id, "page": 0}, isHTML=True)
+        details = HTML.ElementFromURL(TELE_ZUERI_DETAILS_URL, values={"channel_id": ident, "video_id": episode_id, "page": 0})
 
         summary = "\n".join(details.xpath('span[@class="text"]/text()'))
 
